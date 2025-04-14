011ed312aea6811a2331fe4aeaaf707c43d72f5a
Wahyu Amanullah <wahyu.amanullah@gmail.com>
2025-04-14 16:48:57 +0800

    feat(keybind): improve yoink function keybind and update packages

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,27 +14,28 @@ vim.cmd("set autoread")
 -- vim.api.nvim_set_keymap("n", "<leader>pdb", "oimport pdb; pdb.set_trace()<Esc>:w<CR>", { noremap = true, silent = true })
 --
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "python",
-	callback = function()
-		vim.bo.expandtab = true
-		vim.bo.tabstop = 4
-		vim.bo.shiftwidth = 4
-		vim.bo.softtabstop = 4
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>pdb",
-			"oimport pdb; pdb.set_trace()<Esc>:w<CR>",
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_set_keymap("n", "<leader>yf", "bvwhy", { noremap = true, silent = true })
-	end,
+  pattern = "python",
+  callback = function()
+    vim.bo.expandtab = true
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>pdb",
+      "oimport pdb; pdb.set_trace()<Esc>:w<CR>",
+      { noremap = true, silent = true }
+    )
+    -- vim.api.nvim_set_keymap("n", "<leader>yf", "bvwhy", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>yf", "?def <CR>wvf(hy", { noremap = true, silent = true })
+  end,
 })
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "typescript", "javascript" },
-	callback = function()
-		vim.bo.expandtab = true
-		vim.bo.tabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.softtabstop = 2
-	end,
+  pattern = { "typescript", "javascript" },
+  callback = function()
+    vim.bo.expandtab = true
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+  end,
 })
